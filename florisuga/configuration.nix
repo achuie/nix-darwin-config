c838fc437c93e9dd2c7719e81a9143af2b7094ad
achuie <achuie@protonmail.com>
2026-08-12 20:29:53 +0900

    Fix port.

--- florisuga/configuration.nix
+++ florisuga/configuration.nix
@@ -196,7 +196,7 @@
   services.caddy = {
     enable = true;
     virtualHosts = {
-      "florisuga:5092".extraConfig = ''
+      "florisuga:8080".extraConfig = ''
         reverse_proxy 127.0.0.1:5092
         '';
     };
